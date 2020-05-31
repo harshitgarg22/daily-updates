e6189894fe59ef46182280dbee0c354d17a9ffc3
harshitgarg22 <hg1229@gmail.com>
2020-05-31 19:07:25 +0530

    Add daily_update

---
+++ daily_update.js
@@ -0,0 +1,26 @@
+const Discord = require("discord.js");
+const auth = require("./auth.json");
+const client = new Discord.Client();
+
+const data = require("./data.json");
+const ps = data.ps;
+
+client.login(auth.token).then(() => {
+    let guild = auth.teeten.guildid;
+    let channel = guild.channels.get(auth.teeten.channelid["comp-sci"]);
+    if(guild && channel){
+        channel.send("Gentle reminder folks.");
+        let timeMessage = getTime();
+        let duration = ps.endTimestamp - ps.startTimestamp;
+
+        let elapsed = timeMessage - ps.startTimestamp;
+        let elapsedCost = (ps.cost * elapsed) / duration;
+
+        channel.send(
+          "\u20B9" + elapsedCost.toFixed(2) + " are begone."
+        )
+        .then(() => client.destroy());
+    }
+});
+
+client.destroy();
